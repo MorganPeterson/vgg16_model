110841936c02574e8575af9c8d61dbb9bfd73120
MorganPeterson <lastyearsmodel@gmail.com>
2022-07-05 08:51:09 -0400

    reformatted model builder

--- lib/vgg16_model.ex
+++ lib/vgg16_model.ex
@@ -1,8 +1,3 @@
-EXLA.set_as_nx_default(
-  [:tpu, :cuda, :rocm, :host],
-  run_options: [keep_on_device: true]
-)
-
 defmodule VGG16Model do
   @moduledoc """
   Documentation for `Vgg16Model`.
@@ -10,49 +5,47 @@ defmodule VGG16Model do
 
   require Axon
 
-  @doc """
-  Build VGG16 model.
+  defp block_1(input_shape) do
+    input_shape
+    |>Axon.input("input")
+    |> Axon.conv(64, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv1_1")
+    |> Axon.conv(64, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv1_2")
+    |> Axon.max_pool(strides: [2, 2], name: "max_pool_1")
+  end
 
-  ## Examples
+  defp block_2(block) do
+    block
+    |> Axon.conv(128, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv2_1")
+    |> Axon.conv(128, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv2_2")
+    |> Axon.max_pool(strides: [2, 2], name: "max_pool_2")
+  end
 
-      iex> Vgg16Model.build_model({224, 224, 3}, 1000)
-  """
+  defp block_3(block) do
+    block
+    |> Axon.conv(256, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv3_1")
+    |> Axon.conv(256, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv3_2")
+    |> Axon.conv(256, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv3_3")
+    |> Axon.max_pool(strides: [2, 2], name: "max_pool_3")
+  end
 
-  def build_model(input, count) do
-    block1 =
-      Axon.input(input, "input")
-      |> Axon.conv(64, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv1_1")
-      |> Axon.conv(64, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv1_2")
-      |> Axon.max_pool(strides: [2, 2], name: "max_pool_1")
+  defp block_4(block) do
+    block
+    |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv4_1")
+    |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv4_2")
+    |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv4_3")
+    |> Axon.max_pool(strides: [2, 2], name: "max_pool_4")
+  end
 
-    block2 =
-      block1
-      |> Axon.conv(128, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv2_1")
-      |> Axon.conv(128, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv2_2")
-      |> Axon.max_pool(strides: [2, 2], name: "max_pool_2")
+  defp block_5(block) do
+    block
+    |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv5_1")
+    |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv5_2")
+    |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv5_3")
+    |> Axon.max_pool(strides: [2, 2], name: "max_pool_4")
+  end
 
-    block3 =
-      block2
-      |> Axon.conv(256, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv3_1")
-      |> Axon.conv(256, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv3_2")
-      |> Axon.conv(256, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv3_3")
-      |> Axon.max_pool(strides: [2, 2], name: "max_pool_3")
-
-    block4 =
-      block3
-      |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv4_1")
-      |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv4_2")
-      |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv4_3")
-      |> Axon.max_pool(strides: [2, 2], name: "max_pool_4")
-
-    block5 =
-      block4
-      |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv5_1")
-      |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv5_2")
-      |> Axon.conv(512, kernel_size: {3, 3}, padding: :same, activation: :relu, name: "conv5_3")
-      |> Axon.max_pool(strides: [2, 2], name: "max_pool_4")
-
-    block5
+  defp block_encoder(block, count) do
+    block
     |> Axon.flatten(name: "flatten")
     |> Axon.dense(4096, activation: :relu, name: "fc_1")
     |> Axon.dropout(rate: 0.5, name: "dropout_1")
@@ -62,19 +55,50 @@ defmodule VGG16Model do
   end
 
   @doc """
+  Build VGG16 model.
+
+  ## Examples
+
+      iex> Vgg16Model.build_model({nil, 224, 224, 3}, 10)
+  """
+
+  def build_model(input_shape, count) do
+    input_shape
+    |> block_1
+    |> block_2
+    |> block_3
+    |> block_4
+    |> block_5
+    |> block_encoder(count)
+  end
+
+  @doc """
   Train VGG16 model.
 
   ## Examples
 
-      iex> Vgg16Model.train(model, data, 10)
+      iex> Vgg16Model.train(model, data, optimizer, 10)
   """
-
-  def train(model, data, optimizer, epochs) do
+  def train_model(model, data, optimizer, epochs) do
     model
-      |> Axon.Loop.trainer(:categorical_cross_entropy, optimizer)
-      |> Axon.Loop.metric(:accuracy, "Accuracy")
-      |> Axon.Loop.run(data, %{}, epochs: epochs, iterations: 100)
+    |> Axon.Loop.trainer(:categorical_cross_entropy, optimizer, log: 1)
+    |> Axon.Loop.metric(:accuracy)
+    |> Axon.Loop.run(data, %{}, epochs: epochs, iterations: 1)
+  end
+
+  @doc """
+  Test VGG16 model.
+
+  ## Examples
+
+      iex> Vgg16Model.train(model, state, data)
+  """
+  def test_model(model, state, data) do
+    model
+    |> Axon.Loop.evaluator()
+    |> Axon.Loop.metric(:accuracy, "Accuracy")
+    |> Axon.Loop.run(data, state)
   end
 end
 
-VGG16Model.build_model({224, 224, 3}, 1100)
+VGG16Model.build_model({nil, 224, 224, 3}, 1100)
